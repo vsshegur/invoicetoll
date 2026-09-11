5b4cd01140e45313bce9a5762c36b36848cc665e
Vipul Shegur <vsshegur@gmail.com>
2026-09-11 10:34:40 +0530

    Refresh PWA cache after OCR fixes

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='tally-invoice-v3';
-const ASSETS=['./','./index.html','./styles.css','./app-v3.js','./manifest.webmanifest','./icon.svg'];
+const CACHE='tally-invoice-v4';
+const ASSETS=['./','./index.html','./styles.css','./app-v3.js','./ocr-normalizer-v1.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request))) });
